7631b07042c6624435ca59536b7a3334741e7c1f
Aske Wachs <aske@wachs.dk>
2024-02-05 21:21:18 +0100

    populate bitcounter

--- src/algorithms/spatialparquet_java_port.cpp
+++ src/algorithms/spatialparquet_java_port.cpp
@@ -5,10 +5,35 @@
 #include "algorithms/spatialparquet_java_port.hpp"
 #include "helpers/bitstream.hpp"
 #include <iostream>
+#include <bit>
+#include <cmath>
 using std::vector;
 
 namespace algorithms {
     void spatialparquet_java_port::encode(const vector<double_t>& in, vector<byte_t>& out) {
+        std::array<uint32_t, 64> bitCounter {};
+//        std::array<uint32_t, 64> _bitCounter {
+//            0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 6, 4, 2, 3, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+//        };
+        bitCounter.fill(0);
+        int64_t prev = 0;
+        uint64_t totalValues = 0;
+        for (auto&& d : in) {
+            auto l  = *reinterpret_cast<const int64_t *>(&d);
+            auto diff = l - prev;
+            diff = (diff >> 63) ^ (diff << 1);
+            uint8_t leadingZeros = __builtin_clzll(diff);
+            if((-1l >> leadingZeros) == diff || diff == 0){
+                leadingZeros = std::max(leadingZeros - 1, 0);
+            }
+            bitCounter[leadingZeros]++;
+            prev = l;
+            totalValues++;
+        }
+////        auto double_bytes = vector<byte_t>(in.begin(), in.end());
+////        auto data = double_bytes.data();
+//        b.openBytes(reinterpret_cast<const byte_t*>(in.data()), in.size());
+
         throw std::exception{};
     }
     void spatialparquet_java_port::decode(const vector<byte_t>& in, vector<double_t>& out) {
